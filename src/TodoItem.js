21ac70b65f63fac1931e268417fc4d99d1a4b126
Laassari <nlaassari@gmail.com>
2018-08-28 23:50:05 +0100

    display an icon depending on the state (checked/unchecked)

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import checked from './checked.svg';
+import unchecked from './unchecked.svg';
 
-const TodoItem = props => (
-  <p
-    onDragOver={props.handleDragEnter}
-    onDragStart={props.handleDragStart}
-    onDragLeave={props.handleDragLeave}
-    onClick={() => props.markTodoComplete(props.index, !props.completed)}
-    draggable="true"
-    data-index={props.index}
-  >
-    {props.content} is marked: {props.completed.toString()}
-    <button onClick={event => props.deleteTodo(event, props.content)}>
-      delete
-    </button>
-  </p>
-);
+const TodoItem = props => {
+  const ImgIcon = () => (
+    <img src={props.completed ? checked : unchecked} alt="" />
+  );
+  const checkedTextStyles = {
+    opacity: 0.5
+  };
+
+  return (
+    <p
+      onDragOver={props.handleDragEnter}
+      onDragStart={props.handleDragStart}
+      onDragLeave={props.handleDragLeave}
+      onClick={() => props.markTodoComplete(props.index, !props.completed)}
+      draggable="true"
+      data-index={props.index}
+      style={props.completed ? checkedTextStyles : null}
+    >
+      <ImgIcon />
+      {props.content}
+      <button onClick={event => props.deleteTodo(event, props.content)}>
+        delete
+      </button>
+    </p>
+  );
+};
 
 export default TodoItem;
